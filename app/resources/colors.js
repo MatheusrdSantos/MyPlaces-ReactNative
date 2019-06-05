5c02b8a4ecc6c594e5ec3407c056452d6225dc75
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-06-04 22:56:30 -0300

    New folder created to store global app styles

---
+++ app/resources/colors.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    
+});
+
+export const appColors = {
+    primary: '#940000',
+    secondary: '#C30101'
+};
