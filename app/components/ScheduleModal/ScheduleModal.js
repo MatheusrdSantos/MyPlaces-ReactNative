d03e62a4404fc6fe96a9f4e951ccd24d991aa7e8
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-07-02 19:26:25 -0300

    Implement button to confirm modal

--- app/components/ScheduleModal/ScheduleModal.js
+++ app/components/ScheduleModal/ScheduleModal.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import {Animated, Dimensions, TouchableOpacity} from 'react-native';
+import {Animated, Dimensions, TouchableOpacity, TouchableNativeFeedback} from 'react-native';
 import {Icon} from 'native-base';
 import {connect} from 'react-redux';
 import {toggleScheduleModal} from '../../actions';
+import { appColors } from '../../resources/colors';
 
 const screenHeight = Dimensions.get("window").height
 
@@ -41,16 +42,28 @@ class ScheduleModal extends Component {
     render() {
         return (
             <AnimatedContainer style={{ top: this.state.top }}>
-                <Header />
+                <Header>
+                    <Title>Reservar horário</Title>
+                </Header>
                 <TouchableOpacity
                     onPress={this.closeModal}
-                    style={{ position: "absolute", top: 120, left: "50%", marginLeft: -22, zIndex: 1 }}
+                    style={{ position: "absolute", top: 78, left: "75%", marginLeft: -22, zIndex: 1 }}
                 >
                     <CloseView style={{ elevation: 10 }}>
-                        <Icon type="MaterialCommunityIcons" name='close' size={44} color='blue' />
+                        <Icon type="MaterialCommunityIcons" name='close' size={44} style={{color:'white'}}/>
                     </CloseView>
                 </TouchableOpacity>
-                <Body />
+                <TouchableOpacity
+                    onPress={this.closeModal}
+                    style={{ position: "absolute", top: 78, left: "25%", marginLeft: -22, zIndex: 1}}
+                >
+                    <ConfirmView style={{ elevation: 10 }}>
+                        <Icon type="MaterialIcons" name='done' size={44} style={{color:'white'}}/>
+                    </ConfirmView>
+                </TouchableOpacity>
+                <Body>
+
+                </Body>
             </AnimatedContainer>
         )
     }
@@ -67,12 +80,18 @@ const AnimatedContainer = Animated.createAnimatedComponent(Container);
 
 const Header = styled.View`
     background: #333;
-    height: 150px;
+    height: 100px;
+    text-align: center;
 `
-
+const Title = styled.Text`
+    font-size: 18px;
+    color: white;
+    text-align: center;
+    margin-top: 10px;
+`
 const Body = styled.View`
     background: #eaeaea;
-    height: 900px;
+    height: 1000px;
 `
 
 const CloseView = styled.View`
@@ -83,6 +102,17 @@ const CloseView = styled.View`
     justify-content: center;
     align-items: center;
     box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+    background-color: ${appColors.secondary};
+`
+const ConfirmView = styled.View`
+    width: 44px;
+    height: 44px;
+    border-radius: 22px;
+    background: white;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+    background-color: ${appColors.secondary};
 `
 const mapStateToProps = (state) => {
     return {
